441504672c0d43253f0748c40bd390886a1535d2
rikidpr <saezmr@gmail.com>
2015-10-08 22:40:14 +0200

    oauth2 facebook

--- src/main/java/an/dpr/livetracking/services/rest/OAuth2RS.java
+++ src/main/java/an/dpr/livetracking/services/rest/OAuth2RS.java
@@ -1,7 +1,10 @@
 package an.dpr.livetracking.services.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 import javax.annotation.PostConstruct;
 import javax.ws.rs.GET;
@@ -11,31 +14,47 @@ import javax.ws.rs.core.Response;
 
 import org.apache.oltu.oauth2.client.OAuthClient;
 import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthBearerClientRequest;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.client.response.GitHubTokenResponse;
+import org.apache.oltu.oauth2.client.response.OAuthResourceResponse;
+import org.apache.oltu.oauth2.common.OAuth;
 import org.apache.oltu.oauth2.common.OAuthProviderType;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/auth/")
 public class OAuth2RS {
+    
+    private static final Logger log = LoggerFactory.getLogger(OAuth2RS.class);
+    private static final Properties properties = new Properties(); 
 
     private String clientId;
     private String clientSecret;
     private String applicationHost;
     private String callbackUri;
-    private String loginUri;
     private final String FACEBOOK_CODE_ID = "?code=";
+    
+    static {
+	InputStream inputStream = OAuth2RS.class.getResourceAsStream("/oauth.properties");
+	try {
+	    properties.load(inputStream);
+	} catch (IOException e) {
+	    log.error("Error cargando propiedades", e);
+	}
+    }
 
     @PostConstruct
     public void init() {
-	clientId = "131804060198305";
-	clientSecret = "3acb294b071c9aec86d60ae3daf32a93";
-	applicationHost = "localhost:8282";
-	callbackUri = applicationHost + "/auth/facebook/callback";
-	loginUri = applicationHost + "/welcome";
+	clientId = properties.getProperty("facebook.clientId");
+	clientSecret = properties.getProperty("facebook.clientSecret");
+	applicationHost = properties.getProperty("facebook.applicationHost");
+	callbackUri = applicationHost + properties.getProperty("facebook.callbackUri");
     }
+
 
     @GET
     @Path("/facebook")
@@ -46,30 +65,42 @@ public class OAuth2RS {
     }
 
     @GET
-    @Path("/auth/facebook/callback/{code}")
+    @Path("/facebook/callback/{code}")
     public void callback(@PathParam("code") String pCode) throws OAuthSystemException {
 	OAuthClientRequest request;
 	String code = pCode.substring(FACEBOOK_CODE_ID.length() - 1);
 	try {
-	    request = OAuthClientRequest.tokenProvider(OAuthProviderType.FACEBOOK)
-		    .setGrantType(GrantType.AUTHORIZATION_CODE).setClientId(clientId).setClientSecret(clientSecret)
-		    .setRedirectURI(loginUri).setCode(code).buildBodyMessage();
+	    request = OAuthClientRequest
+	                .tokenProvider(OAuthProviderType.FACEBOOK)
+	                .setGrantType(GrantType.AUTHORIZATION_CODE)
+	                .setClientId(clientId)
+	                .setClientSecret(clientSecret)
+	                .setRedirectURI(callbackUri)
+	                .setCode(code)
+	                .buildQueryMessage();
 
 	    OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
 
 	    // Facebook is not fully compatible with OAuth 2.0 draft 10, access
-	    // token response is
+	    // token response isc
 	    // application/x-www-form-urlencoded, not json encoded so we use
 	    // dedicated response class for that
 	    // Own response class is an easy way to deal with oauth providers
 	    // that introduce modifications to
 	    // OAuth specification
 	    GitHubTokenResponse oAuthResponse = oAuthClient.accessToken(request, GitHubTokenResponse.class);
+	    
 
-	    // TODO con el token comprobamos que el idusuario y entramos en la
+	    //TODO si no falla, se da por logeado
 	    // app
-	    System.out.println("Access Token: " + oAuthResponse.getAccessToken() + ", Expires in: "
+	    log.debug("Access Token: " + oAuthResponse.getAccessToken() + ", Expires in: "
 		    + oAuthResponse.getExpiresIn());
+	    
+	    OAuthClientRequest bearerClientRequest = new OAuthBearerClientRequest("https://graph.facebook.com/me")
+	         .setAccessToken(oAuthResponse.getAccessToken()).buildQueryMessage();
+	 
+	    OAuthResourceResponse resourceResponse = oAuthClient.resource(bearerClientRequest, OAuth.HttpMethod.GET, OAuthResourceResponse.class);
+	    System.out.println(resourceResponse.getBody());
 	} catch (OAuthProblemException e) {
 	    System.out.println("OAuth error: " + e.getError());
 	    System.out.println("OAuth error description: " + e.getDescription());
